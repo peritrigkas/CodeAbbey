c69891fd3f37a8efe4a922be91e7f14ae42c532c
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-11-19 23:13:57 +0000

    Sum of an Array using for loop

---
+++ CodeAbbey/Sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAbbey
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+
+            string[] tokens = Console.ReadLine().Split(' ');
+            string[] token = Console.ReadLine().Split(' ');
+           
+            int totalLine = 0;
+            int i;
+           
+
+            for (i = 0; i < token.Length; i++)
+            {
+                totalLine = totalLine + int.Parse(token[i]);
+            }
+
+            
+            Console.WriteLine(totalLine);
+            Console.ReadLine();
+        }
+    }
+}
