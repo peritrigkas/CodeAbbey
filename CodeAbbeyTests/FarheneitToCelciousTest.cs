b2fa2283ff98b1eab52d17c80ed49aba031d7a2a
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-11-23 01:27:27 +0000

    Type of data corrected to int

--- CodeAbbeyTests/FarheneitToCelciousTest.cs
+++ CodeAbbeyTests/FarheneitToCelciousTest.cs
@@ -10,11 +10,11 @@ namespace CodeAbbeyTests
         public void TestMethod1()
         {
             int[] Fahreneit = new int[5];
-            double[] expected = new double[] { -16, 1, 296, 38, 100 };
-            double[] actual = new double[] { -16, 1, 296, 38, 100 };
+            int[] expected = new int[] { -16, 1, 296, 38, 100 };
+            //double[] actual = new double[] { -16, 1, 296, 38, 100 };
             FahrenheitToCelcious celciousTest = new FahrenheitToCelcious();
 
-            celciousTest.Conversion(Fahreneit);
+            int[] actual= celciousTest.Conversion(Fahreneit);
 
             CollectionAssert.AreEqual(expected, actual);
 
